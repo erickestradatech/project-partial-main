86d25dfb1699424bdb181719507bdbc265d91f18
ErickTech <erickestradatech@gmail.com>
2021-04-08 17:09:14 -0500

    completed project

---
+++ src/main/java/clases/Parcial.java
@@ -0,0 +1,37 @@
+package clases;
+
+public class Parcial extends Docente {
+
+    private int horas;
+
+    public Parcial(int codp, String nomp, int aing, int grado, int horas) {
+        super(codp, nomp, aing, grado);
+        this.horas = horas;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    @Override
+    public double pago() {
+
+        if (super.getGrado() == 1) {
+
+            return 25 * horas;
+
+        } else if (super.getGrado() == 2) {
+
+            return 30 * horas;
+
+        } else {
+
+            return 40 * horas;
+        }
+    }
+
+}
